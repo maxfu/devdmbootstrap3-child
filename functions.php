cbc74e64f0eb59b8b2dfb6ce0510c10722e56e44
Xiaoye Fu <fxy83613@gmail.com>
2016-05-20 23:07:22 +1000

    Typo fix in comment

--- functions.php
+++ functions.php
@@ -148,11 +148,11 @@ function basic_wp_seo($keywords) {
 }
 // END Add Basic SEO Function
 
-// Begin Add Cpoyright Year Auto Calculation
+// Begin Add Copyright Year Auto Calculation
 function auto_copyright($year = 'auto') {
    if (intval($year) == 'auto') { $year = date('Y'); }
    if (intval($year) == date('Y')) { echo intval($year); }
    if (intval($year) < date('Y')) { echo intval($year) . ' - ' . date('Y'); }
    if (intval($year) > date('Y')) { echo date('Y'); }
 }
-// END Add Cpoyright Year Auto Calculation
+// END Add Copyright Year Auto Calculation
