1d7005502c380e087e30b196e6ab5c2537fc9f50
Xiaoye Fu <fxy83613@gmail.com>
2016-06-10 20:14:57 +1000

    Fix Search Windows Title

--- functions.php
+++ functions.php
@@ -57,7 +57,7 @@ function add_search_box($items, $args) {
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
+          <h4 class="modal-title">Search</h4>
         </div>
         <div class="modal-body">' . $searchform . '</div>
         <div class="modal-footer">
